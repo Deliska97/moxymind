54270a21022b9df40f63ddb6af42c9f74fd0ca4a
adam.dely <adam.dely@uniqa.at>
2025-05-29 14:03:24 +0200

    Add appium settings

---
+++ wdio.conf.js
@@ -0,0 +1,33 @@
+exports.config = {
+    runner: 'local',
+    specs: ['./appium/specs/**/*.js'],
+    maxInstances: 1,
+
+    hostname: 'localhost',
+    port: 4723,
+    path: '/wd/hub',
+
+    capabilities: [{
+        platformName: 'Android',
+        browserName: 'Chrome',
+        'appium:deviceName': 'Android GoogleAPI Emulator',
+        'appium:platformVersion': '12.0',
+        'appium:automationName': 'UiAutomator2'
+    }],
+
+    logLevel: 'info',
+    bail: 0,
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    services: ['appium'],
+    appium: {
+        command: 'appium'
+    },
+    framework: 'mocha',
+    reporters: ['spec'],
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    }
+};
